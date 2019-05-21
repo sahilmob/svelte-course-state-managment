7520c055887196bfbcce6030c37ec1e199a5ea9b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-05-21 22:29:06 +0300

    use readable store

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,18 @@
   import Cart from "./Cart/Cart.svelte";
   import Products from "./Products/Products.svelte";
   import Button from "./UI/Button.svelte";
+  import timer from "./timer-store";
+  import { onDestroy } from "svelte";
 
   let showCart = true;
+
+  const unsubscribe = timer.subscribe(count => {
+    console.log("App: " + count);
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
 </script>
 
 <Button on:click={() => (showCart = !showCart)}>Toogle Cart</Button>
